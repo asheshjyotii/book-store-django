78d58522602c77fcced7ac7b783897c96f10b410
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2026-08-04 22:57:11 +0530

    Book store app, init, postgresql, superuser

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
-#Pull base image python
+#Pull base image
 FROM python:3.10.4-slim-bullseye
 
-#Set environment variables
+#ENV variables
 ENV PIP_DISABLE_PIP_VERSION_CHECK=1
-ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+ENV PYTHONDONTWRITEBYTECODE=1
 
-#Set work directory
+# workdir
 WORKDIR /code
 
-#Install dependencies
+
+# Requirements
 COPY ./requirements.txt .
-RUN pip install -r requirements.txt
+RUN python -m pip install -r requirements.txt
+
+#COPY all files
 COPY . .
+
--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3lwkieiygk)cv+x^$hj34#&#(9kvin8#1&4ne3n#%t*wqb0rvd'
+SECRET_KEY = 'django-insecure-9zbk=gl17lhjt)bs#q*1zniac#df(lz*s8(c*vmg^u$hr8mdgo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #Local
-    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +73,21 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432, 
     }
 }
 
