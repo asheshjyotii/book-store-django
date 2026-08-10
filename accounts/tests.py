7050f7c08f93858cc028348583321e5249c3ade7
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2026-08-10 12:01:28 +0530

    first unit tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="asheshjyoti1", email="asheshjyoti@gmail.com", password="asheshjyoti@123"
+        )
+        self.assertEquals(user.username,"asheshjyoti1")
+        self.assertEquals(user.email,"asheshjyoti@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        user =User.objects.create_superuser(
+            username="admin", email="admin@gmail.com", password="admin@123"
+        )
+        self.assertEquals(user.username, "admin")
+        self.assertEquals(user.email, "admin@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
