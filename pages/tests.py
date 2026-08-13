74566917b6d4a4d1773dc86da6152517e58b58dc
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2026-08-13 21:50:35 +0530

    added home page

---
+++ pages/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+# check page exists on the route
+    def test_page_exists_at_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+# check the named url correctly reverses back to a page
+    def test_page_exists_at_named_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
