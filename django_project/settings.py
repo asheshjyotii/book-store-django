1bdad6c5af8c54bd5e71b9fcabf3e6278810856b
Ashesh Jyoti Majumdar <121642065+asheshjyotii@users.noreply.github.com>
2026-08-20 22:02:04 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+#. https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
