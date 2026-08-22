275120daad02b4fbbbe67b2c9f4b58c100b2847a
Ashesh Jyoti Majumdar <121642065+asheshjyotii@users.noreply.github.com>
2026-08-22 11:07:27 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+# }.
 
 #Local
 DATABASES = { #new.
